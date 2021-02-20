88dc0c48d8508069fce01010fff3acd0ffcf3bf7
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-02-19 20:52:08 -0800

    add backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '794f5432a4f3b2be192ac45a71624259'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '83aa1a51-481f-4dc4-9c0a-7f58e749a025'
+};
