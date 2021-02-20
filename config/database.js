e030d87c1603b10ed2b2ca0e433f4afe5481bae2
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-02-19 21:15:31 -0800

    fix typo

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
-  if (env("NODE_ENV") === production) {
+  if (env("NODE_ENV") === "production") {
     const config = parse(process.env.DATABASE_URL);
     return {
       defaultConnection: "default",
--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => {
-  if (env("NODE_ENV") === production) {
+  if (env("NODE_ENV") === "production") {
     return {
       upload: {
         provider: "aws-s3",
